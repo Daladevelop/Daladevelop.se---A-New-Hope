cc984c6c8585686ae67900d06bcd92c1ca9dc53e
Henrik Carlsson <henrik.carlsson@me.com>
2011-09-17 17:59:22 +0200

    Test

--- index.php
+++ index.php
@@ -29,24 +29,7 @@
 	</nav>
 
 	<section id="main-content">
-		<?php if ( have_posts() ) : ?>
-			<?php while ( have_posts() ) : ?>
-				<?php the_post(); ?>
-				<article role="article">
-					<?php
-						/*
-							If it's the startpage the article headings is
-							h2, otherwise it's h1
-						*/
-						if (is_front_page()) :?>
-						<h2 role="heading"><?php the_title(); ?></h2>
-						<?php else :?>
-						<h1 role="heading"><?php the_title(); ?></h1>
-						<?php endif; ?>
-						<?php the_content(); ?>
-				</article>
-			<?php endwhile; ?>
-		<?php endif; ?>
+		<h1>DALADEVELOP FTW!!!!!</h1>
 		<?php if (is_front_page()) :?>
 			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Startsidans RSS-box') ); ?>
 		<?php endif; ?>
