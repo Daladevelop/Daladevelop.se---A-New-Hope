afc31e61543f4f2e1bd1f17f4cff3e40f983963e
Henrik Carlsson <henrik.carlsson@me.com>
2011-09-18 02:14:30 +0200

    Various

--- front-page.php
+++ front-page.php
@@ -6,12 +6,8 @@
 		<?php wp_nav_menu(array('container' => false, 'depth' => 1)); ?>
 	</nav>
 
-	<section id="main-content">
+	<section id="frontpage-slider">
 		<?php include(dirname(__FILE__)."/_templates/startpage_slider.php"); ?>
-		
-		<?php if (is_front_page()) :?>
-			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Startsidans RSS-box') ); ?>
-		<?php endif; ?>
 	</section>
 
 	<?php get_footer(); ?>
