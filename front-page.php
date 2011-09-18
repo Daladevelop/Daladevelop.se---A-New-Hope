29a083f803cb3d505a509e9e057594855228a4fa
Emil Österlund <emil@larsemil.se>
2011-09-18 03:56:05 +0200

    frontpage

--- front-page.php
+++ front-page.php
@@ -22,7 +22,7 @@
 		while($oneBlogPost->have_posts()): $oneBlogPost->the_post(); ?>
 			<h2><?php the_title();?></h2>
 			<?php the_excerpt(); ?>
-			<a href="<?php the_permalink();" title="<?php the_title();?>">Läs mer</a>
+			<a href="<?php the_permalink();?>" title="<?php the_title();?>">Läs mer</a>
 			
 		<?php endwhile; ?>
 		
