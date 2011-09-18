c0205bdba90142adeca1a8c57477f3271e07e959
Emil Österlund <emil@larsemil.se>
2011-09-18 02:13:39 +0200

    test

--- _templates/startpage_slider.php
+++ _templates/startpage_slider.php
@@ -6,7 +6,7 @@
 		'post_type' => 'any',
 		'meta_key' => 'show_on_startpage',
 		'meta_value' => 1,
-		'meta_key' => 'priority',
+//		'meta_key' => 'priority',
 		'orderby' => 'meta_value',
 //		'posts_per_page' => 3
 	);
